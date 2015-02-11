f92af7fbcea7b776efe246869d5345a76964d8ad
Stephanie Burns <stephanie.burns@mac.com>
2015-02-11 18:10:34 +0800

    removed the secrets.yml file

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,11 +6,12 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
-    def show
-            @images  = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"]
-            @random_no = rand(4)
-            @random_image = @images[@random_no]
-    end
+  
+  def show
+    @images  = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"]
+    @random_no = rand(4)
+    @random_image = @images[@random_no]
+  end
 
   # GET /questions/1
   # GET /questions/1.json
@@ -43,27 +44,27 @@ class QuestionsController < ApplicationController
 
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
-  def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @question.update(question_params)
+  #       format.html { redirect_to @question, notice: 'Question was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @question }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @question.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
-  def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @question.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -73,6 +74,6 @@ class QuestionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
-      params.require(:question).permit(:name, :question)
+      params.require(:question).permit(:question)
     end
 end
