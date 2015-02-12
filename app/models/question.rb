bd3c739de218d7d4ed5f9ca71fd2e12f8daa5869
Stephanie Burns <stephanie.burns@mac.com>
2015-02-12 09:34:27 +0800

    d

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,3 +2,11 @@ class Question < ActiveRecord::Base
 	  mount_uploader :avatar, AvatarUploader
 end
 
+q = Question.new
+q.avatar = params[:file] # Assign a file like this, or
+q.avatar = File.open('somewhere') # like this
+q.save!
+q.avatar.url # => '/url/to/file.png'
+q.avatar.current_path # => 'path/to/file.png'
+q.avatar.identifier # => 'file.png'
+end
