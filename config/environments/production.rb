36f5db5a1dceb88692c0618840ea2ebac5360c2d
Stephanie Burns <stephanie.burns@mac.com>
2015-02-13 15:49:56 +0800

    Precompiling stuff manually because for some reason Heroku was failing at doing so itself

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   Rails.application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
   STDOUT.sync = true
